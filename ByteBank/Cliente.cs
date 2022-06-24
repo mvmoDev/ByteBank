3275d54bce63349101587547def7c2a681879490
mvmoDev <manoelvitoroliveira@hotmail.com>
2022-06-24 15:44:32 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bytebank.Titular
+{
+    public class Cliente
+    {
+
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+
+        public static int TotalClientesCadastrados { get; set; }
+
+        public Cliente()
+        {
+            TotalClientesCadastrados = TotalClientesCadastrados + 1;
+        }
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+using bytebank;
+
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "André Silva";
+conta1.conta = "110123-X";
+conta1.numero_agencia = 123;
+conta1.nome_agencia = "Agência Central";
+conta1.saldo = 100;
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Amanda Nogueira";
+conta2.conta = "10106-X";
+conta2.numero_agencia = 321;
+conta2.nome_agencia = "Agência Central";
+conta2.saldo = 100;
+
+Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André  pré- Transferência: " + conta1.saldo);
+bool transferencia = conta1.Transferir(50, conta2);
+Console.WriteLine("Transferência realizada com sucesso? " + transferencia);
+Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+Console.WriteLine("Saldo do André  pós- Transferência: " + conta1.saldo);
+
+Cliente sarah = new Cliente();
+sarah.Nome = "Sarah Silva";
+sarah.Profissao = "Professora";
+sarah.Cpf = "11111111-12";
+
+Cliente ester = new Cliente();
+ester.Nome = "Ester Almeida";
+ester.Profissao = "Advogada";
+ester.Cpf = "868524125-32";
+
+Console.WriteLine("Total de clientes: " + Cliente.TotalClientesCadastrados);
+
+ContaCorrente contaAndre = new ContaCorrente(159, "152869-x");
+contaAndre.Titular = new Cliente();
+contaAndre.Titular.Nome = " André Pereira";
+contaAndre.Titular.Profissao = "Auxiliar Administrativo";
+contaAndre.Saldo = 100;
+
+Console.WriteLine("Total de clientes: " + Cliente.TotalClientesCadastrados);
+
+Console.ReadKey();
+
